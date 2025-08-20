a43a6fd41290ee07492876b73979f04345bc57f5
mo <mocompute@outlook.com>
2025-08-21 11:28:20 +1200

    wip

--- src/mos/include/alloc.h
+++ src/mos/include/alloc.h
@@ -13,10 +13,17 @@ typedef struct mos_allocator {
 // Return the default allocator: system's malloc/free
 mos_allocator *mos_alloc_default_allocator();
 
+mos_allocator *mos_alloc_arena_alloc(mos_allocator *parent);
+void           mos_alloc_arena_dealloc(mos_allocator *parent, mos_allocator **arena);
+int            mos_alloc_arena_init(mos_allocator *, mos_allocator *parent, size_t);
+void           mos_alloc_deinit(mos_allocator *);
+
 // -- utilities --
 
-void  mos_alloc_invalidate(void *, size_t);
-char *mos_alloc_strdup(mos_allocator *, char const *);
-char *mos_alloc_strndup(mos_allocator *, char const *, size_t);
+void   mos_alloc_invalidate(void *, size_t);
+char  *mos_alloc_strdup(mos_allocator *, char const *);
+char  *mos_alloc_strndup(mos_allocator *, char const *, size_t);
+
+size_t mos_alloc_next_power_of_two(size_t);
 
 #endif
--- src/mos/src/alloc.c
+++ src/mos/src/alloc.c
@@ -20,6 +20,40 @@ mos_allocator *mos_alloc_default_allocator() {
   return &allocator;
 }
 
+// -- arena --
+
+typedef struct arena_header {
+  struct arena_header *next;
+  size_t               capacity;
+  size_t               size;
+} arena_header;
+
+typedef struct arena_allocator {
+  struct mos_allocator allocator;
+  mos_allocator       *parent;
+  arena_header        *head;
+} arena_allocator;
+
+mos_allocator *mos_alloc_arena_alloc(mos_allocator *alloc) {
+  return alloc->malloc(sizeof(arena_allocator));
+}
+
+void mos_alloc_arena_dealloc(mos_allocator *alloc, mos_allocator **arena) {
+  alloc->free(*arena);
+  *arena = NULL;
+}
+
+int mos_alloc_arena_init(mos_allocator *arena_, mos_allocator *parent, size_t sz) {
+  arena_allocator *arena = (arena_allocator *)arena_;
+  arena->parent          = parent;
+  sz                     = mos_alloc_next_power_of_two(sz);
+  if (0 == sz) return 1;
+  arena->head = parent->malloc(sizeof(arena_header) + sz);
+  if (NULL == arena->head) return 1;
+}
+
+// -- utilities --
+
 char *mos_alloc_strdup(mos_allocator *alloc, char const *src) {
   size_t len = strlen(src);
   char  *out = alloc->malloc(len + 1);
@@ -51,4 +85,22 @@ void mos_alloc_invalidate(void *p, size_t len) {
 #else
   memset(p, 0, len);
 #endif
+}
+
+// Returns: input if already a power of two, or else the next higher
+// power of two.
+size_t mos_alloc_next_power_of_two(size_t n) {
+
+  if (n > (SIZE_MAX / 2) + 1) return 0; // overflow
+  if (n == 0) return 1;
+
+  // set all bits to the right of the highest set bit by masking.
+  n--;
+  n |= n >> 1;
+  n |= n >> 2;
+  n |= n >> 4;
+  n |= n >> 8;
+  n |= n >> 16;
+  n |= n >> 32;
+  return n + 1;
 }
