1ce4b120fd46b709c2e755c80e5fe0bc65c162e6
mo <mocompute@outlook.com>
2025-08-23 17:34:16 +1200

    add float types

--- src/mos/include/types.h
+++ src/mos/include/types.h
@@ -17,4 +17,7 @@ typedef uint16_t      u16;
 typedef uint32_t      u32;
 typedef uint64_t      u64;
 
+typedef float         f32;
+typedef double        f64;
+
 #endif
--- src/mos/src/test_types.c
+++ src/mos/src/test_types.c
@@ -4,6 +4,11 @@
 #include <limits.h>
 
 int main(void) {
+
     assert(8 == CHAR_BIT);
     assert(1 == sizeof(byte));
+    assert(4 == sizeof(float));
+    assert(8 == sizeof(double));
+
+    return 0;
 }
