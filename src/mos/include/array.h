fbb49c2db4eed2086f0bc88b965cfb0cec1411fd
mo <mocompute@outlook.com>
2025-09-06 21:09:27 +1200

    add static asserts to catch use of array macros on non-arrays

--- src/mos/include/array.h
+++ src/mos/include/array.h
@@ -19,6 +19,11 @@ typedef struct {
     u32        capacity
 
 typedef struct {
+    array_header;
+    void *v;
+} array_tmpl;
+
+typedef struct {
     u32 begin;
     u32 end;
 } array_slice_t;
@@ -81,11 +86,15 @@ typedef struct {
 // -- interface --
 
 #define array_reserve(p, n)                                                                                \
-    (p).v = array_reserve_impl((array_header_t *)&(p), (p).v, (n), sizeof(p).v[0], alignof((p).v[0]))
+    do {                                                                                                   \
+        static_assert(sizeof(p) >= sizeof(array_tmpl), "not an array");                                    \
+        (p).v = array_reserve_impl((array_header_t *)&(p), (p).v, (n), sizeof(p).v[0], alignof((p).v[0])); \
+    } while (0)
 
 #define array_push(p, x)                                                                                   \
     do {                                                                                                   \
         static_assert(sizeof((x)[0]) == sizeof((p).v[0]), "size mismatch");                                \
+        static_assert(sizeof(p) >= sizeof(array_tmpl), "not an array");                                    \
         (p).v = array_push_impl((array_header_t *)&(p), (p).v, sizeof(p).v[0], alignof((p).v[0]), (x));    \
     } while (0)
 
@@ -95,11 +104,16 @@ typedef struct {
         array_push((p), &tmp);                                                                             \
     } while (0)
 
-#define array_free(p) array_free_impl((array_header_t *)&(p), (p).v)
+#define array_free(p)                                                                                      \
+    do {                                                                                                   \
+        static_assert(sizeof(p) >= sizeof(array_tmpl), "not an array");                                    \
+        array_free_impl((array_header_t *)&(p), (p).v);                                                    \
+    } while (0)
 
 #define array_copy(p, xs, n)                                                                               \
     do {                                                                                                   \
         static_assert(sizeof((xs)[0]) == sizeof((p).v[0]), "size mismatch");                               \
+        static_assert(sizeof(p) >= sizeof(array_tmpl), "not an array");                                    \
         (p).v = array_copy_impl((array_header_t *)&(p), (p).v, sizeof(p).v[0], alignof((p).v[0]),          \
                                 (void *)(xs), (u32)(n));                                                   \
     } while (0)
@@ -107,6 +121,7 @@ typedef struct {
 #define array_copy_(p, ty, xs, n)                                                                          \
     do {                                                                                                   \
         static_assert(sizeof((xs)[0]) == sizeof(ty), "size mismatch");                                     \
+        static_assert(sizeof(p) >= sizeof(array_tmpl), "not an array");                                    \
         (p).v =                                                                                            \
           array_copy_impl((array_header_t *)&(p), (p).v, sizeof(ty), alignof(ty), (void *)(xs), (u32)(n)); \
     } while (0)
@@ -114,6 +129,7 @@ typedef struct {
 #define array_move(p, xs, n)                                                                               \
     do {                                                                                                   \
         static_assert(sizeof((xs)[0]) == sizeof((p).v[0]), "size mismatch");                               \
+        static_assert(sizeof(p) >= sizeof(array_tmpl), "not an array");                                    \
         (p).v = array_move_impl((array_header_t *)&(p), (p).v, sizeof(p).v[0], alignof((p).v[0]),          \
                                 (void *)(xs), (u32)(n));                                                   \
     } while (0)
@@ -121,15 +137,22 @@ typedef struct {
 #define array_insert(p, i, xs, n)                                                                          \
     do {                                                                                                   \
         static_assert(sizeof((xs)[0]) == sizeof((p).v[0]), "size mismatch");                               \
+        static_assert(sizeof(p) >= sizeof(array_tmpl), "not an array");                                    \
         (p).v = array_insert_impl((array_header_t *)&(p), (p).v, (i), sizeof(p).v[0], alignof((p).v[0]),   \
                                   (xs), (n));                                                              \
     } while (0)
 
 #define array_erase(p, i)                                                                                  \
-    array_erase_impl((array_header_t *)&(p), (p).v, (i), sizeof(p).v[0], alignof((p).v[0]))
+    do {                                                                                                   \
+        static_assert(sizeof(p) >= sizeof(array_tmpl), "not an array");                                    \
+        array_erase_impl((array_header_t *)&(p), (p).v, (i), sizeof(p).v[0], alignof((p).v[0]));           \
+    } while (0)
 
 #define array_shrink(p)                                                                                    \
-    (p).v = array_shrink_impl((array_header_t *)&(p), (p).v, sizeof(p).v[0], alignof(p.v[0]))
+    do {                                                                                                   \
+        static_assert(sizeof(p) >= sizeof(array_tmpl), "not an array");                                    \
+        (p).v = array_shrink_impl((array_header_t *)&(p), (p).v, sizeof(p).v[0], alignof(p.v[0]));         \
+    } while (0)
 
 #define slice_all(x)  {.v = (x).v, .end = (x).size}
 #define slice_size(x) ((x).end - (x).begin)
