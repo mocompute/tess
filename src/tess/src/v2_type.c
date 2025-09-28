31a678beb04f189acf2d76a6feaeeb965accba65
mo <mocompute@outlook.com>
2025-09-28 16:56:30 +1300

    substitutions

--- src/tess/src/v2_type.c
+++ src/tess/src/v2_type.c
@@ -68,11 +68,39 @@ static void tl_type_subs_reserve(tl_type_subs *self, u32 n) {
 
 static void tl_monotype_substitute(tl_monotype *self, tl_type_variable var, tl_monotype mono);
 static void tl_type_scheme_substitute(tl_type_scheme *self, tl_type_variable var, tl_monotype mono);
-static void tl_type_constructor_inst_substitute(tl_type_constructor_inst *self, tl_type_variable var,
-                                                tl_monotype mono);
-static void tl_type_variable_substitute(tl_type_variable *self, tl_type_variable var, tl_monotype mono);
-static void tl_type_arrow_substitute(tl_type_arrow *self, tl_type_variable var, tl_monotype mono);
-static void tl_type_scheme_substitute(tl_type_scheme *self, tl_type_variable var, tl_monotype mono);
+
+static void tl_monotype_substitute(tl_monotype *self, tl_type_variable var, tl_monotype mono) {
+    switch (self->tag) {
+    case tl_cons: {
+        forall(i, self->cons.args) {
+            tl_monotype_substitute(&self->cons.args.v[i], var, mono);
+        }
+    } break;
+
+    case tl_var: {
+        if (self->var == var) *self = mono;
+    } break;
+
+    case tl_arrow: {
+        tl_monotype_substitute(self->arrow.left, var, mono);
+        tl_monotype_substitute(self->arrow.right, var, mono);
+    } break;
+    }
+}
+
+static void tl_monotype_apply_subs(tl_monotype *self, tl_type_subs const *subs) {
+    forall(i, subs->froms) {
+        tl_monotype_substitute(self, subs->froms.v[i], subs->tos.v[i]);
+    }
+}
+
+static void tl_type_scheme_substitute(tl_type_scheme *self, tl_type_variable var, tl_monotype mono) {
+    forall(i, self->quantifiers) {
+        if (var == self->quantifiers.v[i]) return; // do not substitute quantifier
+    }
+
+    tl_monotype_substitute(&self->type, var, mono);
+}
 
 static void tl_type_v2_substitute(tl_type_v2 *self, tl_type_variable var, tl_monotype mono) {
     switch (self->tag) {
@@ -81,15 +109,39 @@ static void tl_type_v2_substitute(tl_type_v2 *self, tl_type_variable var, tl_mon
     }
 }
 
+static void tl_type_v2_apply_subs(tl_type_v2 *self, tl_type_subs const *subs) {
+    forall(i, subs->froms) {
+        tl_type_v2_substitute(self, subs->froms.v[i], subs->tos.v[i]);
+    }
+}
 tl_type_subs *tl_type_subs_compose(allocator *alloc, tl_type_subs const *base, tl_type_subs const *subs) {
 
     tl_type_subs *out = tl_type_subs_create(alloc);
     tl_type_subs_reserve(out, base->froms.size);
 
-    forall(i, base->froms) {
-        tl_type_variable from = base->froms.v[i];
-        tl_monotype      to   = base->tos.v[i];
+    array_copy(out->froms, base->froms.v, base->froms.size);
+    array_copy(out->tos, base->tos.v, base->tos.size);
+
+    // apply subs to all monotypes in base
+    forall(i, out->tos) {
+        tl_monotype_apply_subs(&out->tos.v[i], subs);
+    }
+
+    // copy remaining substitutions where type var is not in base
+    forall(i, subs->froms) {
+        tl_type_variable from = subs->froms.v[i];
+
+        // TODO optimize inner loop
+        if (array_contains(out->froms, from)) continue;
+        array_push(out->froms, from);
+        array_push(out->tos, subs->tos.v[i]);
     }
 
     return out;
+}
+
+void tl_type_env_subs_apply(tl_type_env *env, tl_type_subs const *subs) {
+    forall(i, env->types) {
+        tl_type_v2_apply_subs(&env->types.v[i], subs);
+    }
 }
