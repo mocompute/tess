2580c9919c58ad141c68615c66c3739d96eb063f
mo <mocompute@outlook.com>
2026-01-31 12:07:37 +1300

    don't runtime error on non concrete type

--- src/tess/src/infer.c
+++ src/tess/src/infer.c
@@ -2367,8 +2367,7 @@ static str specialize_type_constructor_(tl_infer *self, str name, tl_monotype_si
             }
 
             (void)specialize_type_constructor_(self, generic_name, args.v[i]->cons_inst->args, &poly, seen);
-            if (poly) {
-                // Note: it's a runtime error if poly is not concrete
+            if (poly && tl_polytype_is_concrete(poly)) {
                 args.v[i] = tl_polytype_concrete(poly);
             }
         }
